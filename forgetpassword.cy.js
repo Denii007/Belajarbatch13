0e1ea7f3f84ceb84331d6121085969d5a55eae39
deni <dekurniawannn@gmail.com>
2025-12-14 20:36:11 +0700

    forget password script automation

---
+++ forgetpassword.cy.js
@@ -0,0 +1,11 @@
+describe('Login Page', () => {
+
+    it('should load the login page', () => {
+        cy.visit('/login');
+        cy.contains('Login to Your Account').should('be.visible');
+        cy.get('input[name="username"]').should('be.visible');
+        cy.get('input[name="password"]').should('be.visible');
+        cy.get('button[type="submit"]').should('be.visible');
+    }); 
+
+});
